b1d2e0a664b073f928809508e1f70b32e13dabec
JoshuaSandvick <josh.sandvick@gmail.com>
2026-08-28 12:02:59 -0400

    Update remote_server.py

--- remote_server.py
+++ remote_server.py
@@ -1,9 +1,12 @@
 import os
 
+from bootstrap_tokens import bootstrap_user_token
 from kroger_mcp.server import create_server
 
 
 def main():
+    bootstrap_user_token()
+
     mcp = create_server()
 
     port = int(os.environ.get("PORT", "10000"))
